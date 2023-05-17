de37075e1d84a4d9ab92faf3ef33b9c0215d0dcd
ulisesrcm <ulises@pukara.dev>
2023-05-16 22:51:47 -0300

    first commit

---
+++ range_vision.js
@@ -0,0 +1,42 @@
+const digits = [3, 4, 3, 1, 5];
+
+function range_vision(number) {
+  let foundIndex = false;
+  let leftRange = [];
+  let rightRange = [];
+
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] === number) {
+      foundIndex = i;
+      break;
+    }
+  }
+
+  if (foundIndex === false) {
+    return null; // El número no está presente en el array
+  }
+
+  for (let j = foundIndex - number; j < foundIndex; j++) {
+    if (j >= 0) {
+      leftRange.push(digits[j]);
+    }
+  }
+
+  for (let k = foundIndex + 1; k <= foundIndex + number; k++) {
+    if (k < digits.length) {
+      rightRange.push(digits[k]);
+    }
+  }
+
+  return {
+    leftRange,
+    rightRange,
+  };
+}
+
+console.log(range_vision(3));
+console.log(range_vision(4));
+console.log(range_vision(3));
+console.log(range_vision(1));
+console.log(range_vision(5));
+
